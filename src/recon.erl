31e5f8873ad4f39750d7e4a94665a59620d2da20
Fred Hebert <mononcqc@ferd.ca>
2013-05-20 18:03:00 -0400

    Reworking structure, adding info/1-3

--- src/recon.erl
+++ src/recon.erl
@@ -1,34 +1,47 @@
 -module(recon).
--export([reductions/2]).
+-export([info/1,info/3,reductions/2]).
 
+%%%%%%%%%%%%%%%%%%
+%%% PUBLIC API %%%
+%%%%%%%%%%%%%%%%%%
+
+%% Allows to be similar to process_info/1, but excludes
+%% the mailbox, which has a tendency to grow. links could still
+%% be problematic for size, but not nearly as much as messages.
+info(A,B,C) -> info(pid(A,B,C)).
+
+-spec info(pid()|atom()) -> [{atom(), [{atom(),term()}]},...].
+info(Name) when is_atom(Name) ->
+    info(whereis(Name));
+info(Pid) when is_pid(Pid) ->
+    Info = fun(List) ->
+        [{Key, Val} || Key <- List,
+                       {_,Val} <- [erlang:process_info(Pid, Key)]]
+    end,
+    [{meta, Info([registered_name, dictionary, group_leader, status])},
+     {signals, Info([links, monitors, monitored_by, trap_exit])},
+     {location, Info([initial_call, current_stacktrace])},
+     {memory, Info([memory, message_queue_len, heap_size, total_heap_size,
+                    garbage_collection])},
+     {work, Info([reductions])}].
+
+%% Fetches reduction counts of processes over a sliding window
 -spec reductions(non_neg_integer(), pos_integer()) -> [term()].
 reductions(Time, Num) ->
-    lists:sublist(
-        lists:usort(
-            fun({_, {X,_,_}}, {_, {Y,_,_}}) -> X > Y end,
-            dict:to_list(build(fetch(Time)))),
-        Num).
+    Sample = fun() ->
+        [{Pid, Reds, {Curr, Init}}
+         || Pid <- processes(),
+            {_, Reds} <- [process_info(Pid, reductions)],
+            {_, Curr} <- [process_info(Pid, current_function)],
+            {_, Init} <- [process_info(Pid, initial_call)]]
+    end,
+    {First,Last} = recon_lib:sample(Time, Sample),
+    lists:sublist(lists:usort(recon_lib:sliding_window(First, Last)), Num).
 
-fetch(Delay) ->
-    Sample1 = sample(),
-    timer:sleep(Delay),
-    Sample2 = sample(),
-    {Sample1, Sample2}.
-
-build({Sample1, Sample2}) ->
-    lists:foldl(
-        fun({Pid, {Red, Current, Init}}, Dict) ->
-                dict:update(Pid,
-                            fun({OldRed,_,_}) -> {Red-OldRed, Current, Init} end,
-                            {Red, Current, Init},
-                            Dict)
-        end,
-        dict:from_list(Sample1),
-        Sample2).
-
-sample() ->
-    [{Pid, {Reds, Curr, Init}}
-     || Pid <- processes(),
-        {_, Reds} <- [process_info(Pid, reductions)],
-        {_, Curr} <- [process_info(Pid, current_function)],
-        {_, Init} <- [process_info(Pid, initial_call)]].
+%%%%%%%%%%%%%%%
+%%% PRIVATE %%%
+%%%%%%%%%%%%%%%
+pid(X, Y, Z) ->
+    list_to_pid("<" ++ integer_to_list(X) ++ "." ++
+		integer_to_list(Y) ++ "." ++
+		integer_to_list(Z) ++ ">").
---
+++ src/recon_lib.erl
@@ -0,0 +1,39 @@
+-module(recon_lib).
+-export([sliding_window/2, sample/2, count/1]).
+
+-type diff() :: [{Key::term(), Diff::number(), Other::term()}].
+
+%% Compare two samples and return a list based on some key
+-spec sliding_window(First::diff(), Last::diff()) -> [diff()].
+sliding_window(First, Last) ->
+    Dict = lists:foldl(
+        fun({Key, {Current, Other}}, Acc) ->
+            dict:update(Key,
+                        fun({Old,_Other}) -> {Current-Old, Other} end,
+                        {Current, Other},
+                        Acc)
+        end,
+        dict:from_list([{K,{V,O}} || {K,V,O} <- First]),
+        [{K,{V,O}} || {K,V,O} <- Last]
+    ),
+    [{K,V,O} || {K,{V,O}} <- dict:to_list(Dict)].
+
+%% Runs a fun over time and returns both samples.
+-spec sample(Ms::non_neg_integer(), fun(() -> term())) ->
+      {First::term(), Second::term()}.
+sample(Delay, Fun) ->
+    First = Fun(),
+    timer:sleep(Delay),
+    Second = Fun(),
+    {First, Second}.
+
+%% Counts similar entries in a dict
+-spec count([term()]) -> [{Count::integer(), term()}].
+count(Terms) ->
+    Dict = lists:foldl(
+        fun(Val, Acc) ->  dict:update_counter(Val, 1, Acc) end,
+        dict:new(),
+        Terms
+    ),
+    dict:to_list(Dict).
+
