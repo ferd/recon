b48bad16f976b26fe0d42add1f3dd111dbaf435c
Fred Hebert <mononcqc@ferd.ca>
2013-09-09 23:21:52 -0400

    Adding docs, fixing APIs
    We drop the dump format support -- not useful and conflicting if only
    part of the functions can support it (i.e. not memory/1)

--- src/recon_alloc.erl
+++ src/recon_alloc.erl
@@ -1,5 +1,14 @@
-%%% Functions to deal with Erlang's memory allocators
+%%% @doc Functions to deal with Erlang's memory allocators
 %%% http://www.erlang.org/doc/man/erts_alloc.html
+%%% This module is not to be considered stable yet.
+%%%
+%%% Glossary:
+%%%  - `sbcs': single-block carriers.
+%%%  - `mbcs': multiblock carriers.
+%%%
+%%% All sizes returned by this module are in bytes.
+%%%
+%%% @todo extend definitions and docs
 -module(recon_alloc).
 %% specific allocators. Not included: 
 %% - sys_alloc (malloc)
@@ -15,27 +24,52 @@
                      std_alloc      % other memory blocks
                     ]).
 
+-type allocator() :: temp_alloc | eheap_alloc | binary_alloc | ets_alloc
+                   | driver_alloc | sl_alloc | ll_alloc | fix_alloc
+                   | std_alloc.
+-type instance() :: non_neg_integer().
+-type allocdata(T) :: {{allocator(), instance()}, T}.
+-export_type([allocator/0, instance/0, allocdata/1]).
+
 %% sbcs: single-block carriers
 %% mbcs: multiblock carriers
 -define(CURRENT_POS, 2). % pos in sizes tuples
 -define(MAX_POS, 4). % pos in sizes tuples
 
--export([memory/1,
-         fragmentation/1, fragmentation/2,
-         cache_hit_rates/0, cache_hit_rates/1,
-         average_sizes/0, average_sizes/1
-         ]).
--export([dump_to_allocators/1]).
+-export([memory/1, fragmentation/1, cache_hit_rates/0, average_sizes/0]).
 
-%% @doc the value for `memory(allocated)' value should match the one
-%% reported by the OS. Otherwise, memory has to be allocated outside
-%% of the regular allocators -- a leak in NIFs or Drivers maybe.
+%%%%%%%%%%%%%%
+%%% Public %%%
+%%%%%%%%%%%%%%
+
+%% @doc reports one of multiple possible memory values for the entire
+%% node depending on what is to be reported:
+%%
+%% - `memory(used)' reports the memory that is actively used for allocated
+%%   Erlang data;
+%% - `memory(allocated)' reports the memory that is reserved by the VM. It
+%%   includes the memory used, but also the memory yet-to-be-used but still
+%%   given by the OS. This is the amount you want if you're dealing with
+%%   ulimit and OS-reported values.
+%% - `memory(unused)' reports the amount of memory reserved by the VM that
+%%   is not being allocated. Equivalent to `memory(allocated) - memory(used)'.
+%% - `memory(usage)' returns a percentage (0.0 .. 1.0) of `used/allocated'
+%%   memory ratios.
+%%
+%% The memory reported by `memory(allocated)' should roughly
+%% match what the OS reports. If this amount is different by a large margin,
+%% it may be the sign that someone is allocating memory in C directly, outside
+%% of Erlang's own allocator -- a big warning sign.
+%%
+%% Also note that low memory usages can be the sign of fragmentation in
+%% memory, in which case exploring which specific allocator is at fault
+%% is recommended (see {@link fragmentation/1})
 -spec memory(used | allocated | unused) -> pos_integer()
     ;       (usage) -> number().
 memory(used) ->
     erlang:memory(total);
 memory(allocated) ->
-    AllocProps = [Prop || {_Alloc,_N,Prop} <- allocators()],
+    AllocProps = [Prop || {_Alloc,Prop} <- allocators()],
     lists:sum(lists:map(fun(Props) ->
         SbcsProps = proplists:get_value(sbcs, Props),
         MbcsProps = proplists:get_value(mbcs, Props),
@@ -48,12 +82,22 @@ memory(unused) ->
 memory(usage) ->
     memory(used) / memory(allocated).
 
-%% compare the current block size to the carrier size of each
-%% allocator instance for both mbcs and sbcs
-%% We should sort for both low % of usage and large total usage, in
-%% order to find relevant leaky allocators.
-fragmentation(Keyword) -> fragmentation(Keyword, allocators()).
-fragmentation(Keyword, Allocs) ->
+%% @doc Compares the block sizes to the carrier sizes, both for
+%% single block (`sbcs') and multiblock (`mbcs') carriers.
+%%
+%% The returned results are sorted by a weight system that is
+%% somewhat likely to return the most fragmented allocators first,
+%% based on their percentage of use and the total size of the carriers,
+%% for both `sbcs' and `mbcs'.
+%%
+%% The values can both be returned for `current' allocator values, and
+%% for `max' allocator values. The current values hold the present allocation
+%% numbers, and max values, the values at the peak. Comparing both together
+%% can give an idea of whether the node is currently being at its memory peak
+%% when possibly leaky, or if it isn't. This information can in turn
+%% influence the tuning of allocators to better fit sizes.
+-spec fragmentation(current | max) -> [allocdata([{atom(), term()}])].
+fragmentation(Keyword) ->
     Pos = case Keyword of
         current -> ?CURRENT_POS;
         max -> ?MAX_POS
@@ -68,13 +112,19 @@ fragmentation(Keyword, Allocs) ->
       {Weight, Vals} = weighed_values({BlockSbcs,CarSbcs},
                                       {BlockMbcs,CarMbcs}),
       {Weight, {Allocator,N}, Vals}
-    end || {Allocator, N, Props} <- Allocs],
+    end || {{Allocator, N}, Props} <- allocators()],
     [{Key,Val} || {_W, Key, Val} <- lists:reverse(lists:sort(WeighedData))].
 
+%% @doc looks at the `mseg_alloc' allocator (allocator used by all the
+%% allocators in {@link allocator()}) and returns information relative to
+%% the cache hit rates. Unless memory has expected spiky behaviour, it should
+%% usually be above 0.80 (80%).
+%% The values returned are sorted by a weight combining the lower cache hit
+%% joined to the largest memory values allocated.
+-spec cache_hit_rates() -> [{{instance,instance()}, [{Key,Val}]}] when
+    Key :: hit_rate | hits | alloc,
+    Val :: term().
 cache_hit_rates() ->
-    cache_hit_rates(erlang:system_info({allocator,mseg_alloc})).
-
-cache_hit_rates(Instances) ->
     WeighedData = [begin
       Mem = proplists:get_value(memkind, Props),
       {_,Hits} = lists:keyfind(cache_hits, 1, proplists:get_value(status,Mem)),
@@ -82,13 +132,21 @@ cache_hit_rates(Instances) ->
       HitRate = usage(Hits,Alloc),
       Weight = (1.00 - HitRate)*Alloc,
       {Weight, {instance,N}, [{hit_rate,HitRate}, {hits,Hits}, {alloc,Alloc}]}
-    end || {instance, N, Props} <- Instances],
+    end || {instance, N, Props} <- erlang:system_info({allocator,mseg_alloc})],
     [{Key,Val} || {_W,Key,Val} <- lists:reverse(lists:sort(WeighedData))].
 
-average_sizes() -> average_sizes(allocators()).
-
-average_sizes(Allocs) ->
-    Dict = lists:foldl(fun({Instance,_,Props},Dict0) ->
+%% @doc Checks all allocators in {@link allocator()} and returns the average
+%% carrier sized being used for `mbcs' and `sbcs'. This value is interesting
+%% to use because it will tell us how used most carriers are individually being
+%% used for. This can be related to the VM's largest multiblock carrier size
+%% (`lmbcs') and smallest multiblock carrier size (`smbcs') to specify
+%% allocation strategies regarding the block sizes to be used.
+%% @todo explain what values could lead to what strategies
+-spec average_sizes() -> [{allocator(), [{Key,Val}]}] when
+    Key :: mbcs | sbcs,
+    Val :: number().
+average_sizes() ->
+    Dict = lists:foldl(fun({{Instance,_},Props},Dict0) ->
       LS = proplists:get_value(sbcs, Props),
       CarSbcs = element(?CURRENT_POS, lists:keyfind(carriers,1,LS)),
       SizeSbcs = element(?CURRENT_POS, lists:keyfind(carriers_size,1,LS)),
@@ -102,39 +160,22 @@ average_sizes(Allocs) ->
       Dict4
     end,
     dict:new(),
-    Allocs),
+    allocators()),
     average_group(average_calc(lists:sort(dict:to_list(Dict)))).
 
-average_calc([]) ->
-    [];
-average_calc([{{Instance,Type,count},Ct},{{Instance,Type,size},Size}|Rest]) ->
-    case {Size,Ct} of
-        {0,0} -> [{Instance, Type, 0} | average_calc(Rest)];
-        _ -> [{Instance,Type,Size/Ct} | average_calc(Rest)]
-    end.
-
-average_group([]) -> [];
-average_group([{Instance,Type1,N},{Instance,Type2,M} | Rest]) ->
-    [{Instance,[{Type1,N},{Type2,M}]} | average_group(Rest)].
-
+%% @doc returns a dump of all allocator settings and values
+-spec allocators() -> [allocdata(term())].
 allocators() ->
-    [{A,N,Props} || A <- ?ALLOCATORS,
-                    {_,N,Props} <- erlang:system_info({allocator,A})].
+    [{{A,N},Props} || A <- ?ALLOCATORS,
+                      {_,N,Props} <- erlang:system_info({allocator,A})].
 
-%% Convert an existing allocator dump to a format this module can manage
-dump_to_allocators([]) -> [];
-dump_to_allocators([{Name, Instances}|Rest]) ->
-    case lists:member(Name, ?ALLOCATORS) of
-        true ->
-            [{Name,N,Props} || {instance,N,Props} <- Instances]
-            ++
-            dump_to_allocators(Rest);
-        false ->
-            dump_to_allocators(Rest)
-    end.
+%%%%%%%%%%%%%%%
+%%% Private %%%
+%%%%%%%%%%%%%%%
 
-%%% Private
-%% sort on small usage vs. large size.
+%% Sort on small usage vs. large size.
+%% The weight cares about both the sbcs and mbcs values, and also
+%% returns a proplist of possibly interesting values.
 weighed_values({SbcsBlockSize, SbcsCarrierSize},
                {MbcsBlockSize, MbcsCarrierSize}) ->
     SbcsUsage = usage(SbcsBlockSize, SbcsCarrierSize),
@@ -149,6 +190,24 @@ weighed_values({SbcsBlockSize, SbcsCarrierSize},
               {mbcs_block_size, MbcsBlockSize},
               {mbcs_carriers_size, MbcsCarrierSize}]}.
               
+%% Returns the `BlockSize/CarrierSize' as a 0.0 -> 1.0 percentage,
+%% but also takes 0/0 to be 100% to make working with sorting and
+%% weights simpler.
 usage(0,0) -> 1.00;
 %usage(N,0) -> ???;
 usage(Block,Carrier) -> Block/Carrier.
+
+%% Calculation for the average of blocks being used.
+average_calc([]) ->
+    [];
+average_calc([{{Instance,Type,count},Ct},{{Instance,Type,size},Size}|Rest]) ->
+    case {Size,Ct} of
+        {0,0} -> [{Instance, Type, 0} | average_calc(Rest)];
+        _ -> [{Instance,Type,Size/Ct} | average_calc(Rest)]
+    end.
+
+%% Regrouping/merging values together in proplists
+average_group([]) -> [];
+average_group([{Instance,Type1,N},{Instance,Type2,M} | Rest]) ->
+    [{Instance,[{Type1,N},{Type2,M}]} | average_group(Rest)].
+
