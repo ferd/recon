4e7c387a4c123e2d2d92ef9547b534d99a3f64e4
Bartlomiej Górny <bartlomiej.gorny@erlang-solutions.com>
2019-03-06 15:59:42 +0100

    nicer formatting of sets

--- src/recon_rec.erl
+++ src/recon_rec.erl
@@ -99,6 +99,8 @@ limit(Name, Arity, Limit) ->
 
 %% @private if a tuple is a known record, formats is as "#recname{field=value}", otherwise returns
 %% just a printout of a tuple.
+format_tuple(Tuple) when element(1, Tuple) == set ->
+    ["set:", recon_trace:format_trace_output(true, sets:to_list(Tuple))];
 format_tuple(Tuple) ->
     ensure_table_exists(),
     First = element(1, Tuple),
